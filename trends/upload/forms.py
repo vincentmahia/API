5847cbb519f5f34c36d0c36a64be83d450bcb063
vincentmahia <vincentmahia123@gmail.com>
2025-03-14 14:18:13 +0300

    Delete trends directory

--- trends/upload/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class FileUploadForm(forms.Form):
-    file = forms.FileField(label="Upload csv/Excel file")
-    target = forms.CharField(max_length=200, label='Target column', widget=forms.TextInput(attrs={'placeholder':'Enter target column'}))
-    
--- trends/upload/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import file_upload_view
-
-urlpatterns = [
-    path('', file_upload_view, name="file_upload"),
-]
